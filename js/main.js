ec420a7480fbc15d557b880e0776ac2041cca34d
孙剑宇 <981152591@qq.com>
2020-12-14 18:03:07 +0800

    Site updated: 2020-12-14 18:03:07

--- js/main.js
+++ js/main.js
@@ -134,5 +134,4 @@
         $mask.on('click', toggleToc);
         $('.navbar-main .catalogue').on('click', toggleToc);
     }
-	
 }(jQuery, window.moment, window.ClipboardJS, window.IcarusThemeSettings));
